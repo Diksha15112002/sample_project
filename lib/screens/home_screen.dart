31317f8ed97205529b6018c8afa11b88706f8bdc
dikshahikare <dikshahikare15@gmail.com>
2025-01-02 12:51:20 +0000

    buttons added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,8 +15,37 @@ class _HomeScreenState extends State<HomeScreen> {
           centerTitle: true,),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ElevatedButton(
+                  onPressed: ()
+                            => context.go('/details'),
+                          child: Text('Elevated Button'),
+                        ),
+                  TextButton(
+                  onPressed: ()
+                  =>context.go('/dialog'),
+                  child: Text('Text Button'),
+                  ),
+                  OutlinedButton(
+                  onPressed: ()
+                  =>context.go('/drawer'),
+                  child: Text('Outlined Button'),
+                  ),
+      IconButton(
+        onPressed: ()
+          =>context.go('/details'),
+        icon: Icon(Icons.details_outlined),
+      ),
+                ],
+              ),
+            ),
+
+  SizedBox(height: 120),
             ElevatedButton(
               onPressed: () => context.go('/details'),
               child: Text('Go to Details'),
@@ -37,6 +66,14 @@ class _HomeScreenState extends State<HomeScreen> {
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
         ],
+      ),
+
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+        },
+        icon: Icon(Icons.home_outlined),
+        label: Text('home'),
+        backgroundColor: Colors.blue,
       ),
     );
   }
